53ca391cbf2936c6a5ffa2b5a7afeaf625bdbb19
SalehC77 <saleh774361224@gmail.com>
2025-07-27 17:52:33 +0300

    init the first linq

---
+++ LINQ 01/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LINQ_01;
+
+Console.WriteLine("Hello, World!");
+
+List<Student> students = new() {
+    new() {Id = 1 , Name = "Saleh" ,Age = 20 ,Gender = true,Phone = "774361224", Gmail = "salehbenjahlan@gmail.com"},
+    new() {Id = 2 , Name = "Saleh" ,Age = 24 ,Gender = true,Phone = "774361224", Gmail = "salehbenjahlan@gmail.com"},
+    new() {Id = 3 , Name = "Ali" ,Age = 80 ,Gender = true,Phone = "774361224", Gmail = "salehbenjahlan@gmail.com"},
+    new() {Id = 4 , Name = "Salim" ,Age = 11 ,Gender = false,Phone = "774361224", Gmail = "salehbenjahlan@gmail.com"},
+    new() {Id = 5 , Name = "Mohammed" ,Age = 44 ,Gender = false,Phone = "774361224", Gmail = "salehbenjahlan@gmail.com"},
+    new() {Id = 6 , Name = "Abdullah" ,Age = 25 ,Gender = false,Phone = "774361224", Gmail = "salehbenjahlan@gmail.com"},
+    new() {Id = 7 , Name = "khaled",Age = 33 ,Gender = false,Phone = "774361224", Gmail = "salehbenjahlan@gmail.com"},
+    new() {Id = 8 , Name = "Fatima" ,Age = 20 ,Gender = true,Phone = "774361224", Gmail = "salehbenjahlan@gmail.com"},
+};
+
+var SqlQerey = from s in students
+             where s.Age > 20
+             select s;
+
+var MethodQerey = students.Where(s => s.Age > 20 && s.Gender == false).Select(s=> s);
+
+Student.PrintAll(MethodQerey);
---
+++ LINQ 01/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_01
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string Gmail { get; set; }
+        public bool Gender { get; set; }
+        
+        public static void PrintAll(IEnumerable<Student> students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Id = {student.Id}, Name = {student.Name} , Age = {student.Age} , Gender = {student.Gender}");
+            }  
+        }
+        public static void Print(Student student)
+        {
+            Console.WriteLine($"Id = {student.Id}, Name = {student.Name} , Age = {student.Age}, Gender = {student.Gender}");
+        }
+    }
+}
